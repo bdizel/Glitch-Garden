f11bea2b4a37b569f0ceb1535146027ffe2ab6db
bdizel <32436426+bdizel@users.noreply.github.com>
2017-10-21 20:13:52 -0700

    Added Fade In to Start Menu, added levels 2/3 and covered all levels with Grass Texture

---
+++ Assets/Scripts/FadeIn.cs
@@ -0,0 +1,37 @@
+﻿/// <summary>
+/// /*FadeIn.cs
+/// Glitch Garden
+/// Brian Douglass
+/// 10.21.17
+/// */
+/// </summary>
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FadeIn : MonoBehaviour {
+	[SerializeField]float _fadeInTime;
+	Image _fadePanel;
+	void Awake(){
+		_fadePanel = GetComponent<Image> ();
+		print ("Fade panel = " + _fadePanel);
+	}
+	void Start(){
+		_fadePanel.CrossFadeAlpha (0f, _fadeInTime, false);	
+	}
+	void FixedUpdate(){
+	}
+	void Update(){
+		if (Time.timeSinceLevelLoad > _fadeInTime)
+			gameObject.SetActive (false);
+	}
+	void LateUpdate(){
+	}
+	void OnDrawGizmos(){
+	}
+	void OnGUI(){
+	}
+}
