1eeb46aace1431120f569b2f7ce4884972bfc43c
bdizel <32436426+bdizel@users.noreply.github.com>
2017-10-21 21:11:13 -0700

    Added Player Prefs Manager

--- Assets/Scripts/FadeIn.cs
+++ Assets/Scripts/FadeIn.cs
@@ -3,30 +3,27 @@
 /// Glitch Garden
 /// Brian Douglass
 /// 10.21.17
-/// */
+/// Fade out the Black Panel UI element that is over everything in the start menu*/
 /// </summary>
 
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class FadeIn : MonoBehaviour {
-	[SerializeField]float _fadeInTime;
-	Image _fadePanel;
+	[SerializeField]float _fadeInTime;										//Editor accessible fade in time.
+	Image _fadePanel;														//Access the image in the panel UI object
 	void Awake(){
-		_fadePanel = GetComponent<Image> ();
-		print ("Fade panel = " + _fadePanel);
+		_fadePanel = GetComponent<Image> ();								//Get the panel image
+	//	print ("Fade panel = " + _fadePanel);
 	}
 	void Start(){
-		_fadePanel.CrossFadeAlpha (0f, _fadeInTime, false);	
+		_fadePanel.CrossFadeAlpha (0f, _fadeInTime, false);					//Change the alpha channel of the image to 0 over _fadeInTime in seconds.
 	}
 	void FixedUpdate(){
 	}
 	void Update(){
-		if (Time.timeSinceLevelLoad > _fadeInTime)
-			gameObject.SetActive (false);
+		if (Time.timeSinceLevelLoad > _fadeInTime)							//Check to see if the _fadeInTime has elapsed
+			gameObject.SetActive (false);									//If so turn off the Panel so the UI elements underneath can be accessed
 	}
 	void LateUpdate(){
 	}
---
+++ Assets/Scripts/PlayerPrefsManager.cs
@@ -0,0 +1,43 @@
+﻿/// <summary>
+/// /*PlayerPrefsManager.cs
+/// Glitch Garden
+/// Brian Douglass
+/// 10.21.17
+/// A wrapper for accessing the player prefences in Unity*/
+/// </summary>
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerPrefsManager : MonoBehaviour {
+	const string MASTER_VOLUME_KEY = "master_volume";
+	const string DIFFICULTY_KEY = "difficulty";
+	const string LEVEL_KEY = "level_unlocked_";
+	void Awake(){
+	}
+	void Start(){
+	}
+	void FixedUpdate(){
+	}
+	void Update(){
+	}
+	void LateUpdate(){
+	}
+	void OnDrawGizmos(){
+	}
+	void OnGUI(){
+	}
+	public static void SetMasterVolume(float volume){
+		if(volume > 0f && volume < 1f){
+			PlayerPrefs.SetFloat (MASTER_VOLUME_KEY, volume);
+		}else{
+			Debug.LogError ("Master volume out of range");
+		}
+	}
+	public static float GetMasterVolume(){
+		return PlayerPrefs.GetFloat (MASTER_VOLUME_KEY);
+	}
+}
